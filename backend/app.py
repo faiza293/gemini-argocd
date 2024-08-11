d6e7e3d6757232add712c17e0fcb968eb0212113
faiza293 <faizafaazmukve@gmail.com>
2024-08-11 20:03:04 +0000

    deployment and service

--- backend/app.py
+++ backend/app.py
@@ -39,5 +39,5 @@ def generate():
     return render_template('index.html', generated_text=generated_text)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT',80))
+    port = int(os.environ.get('PORT',8080))
     app.run(host='0.0.0.0', port=port, debug=True)
